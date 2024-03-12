afff69d32d9a53e1dfeb2c4c9d0e841eb3406bc1
Yu-Hoto <yu.hoto.0220@gmail.com>
2024-03-12 12:14:01 +0900

    remove unnecessary var.

--- example.rb
+++ example.rb
@@ -2,9 +2,6 @@ require 'html_pipeline'
 require './auto_link_filter.rb'
 require './new_line_filter.rb'
 
-autolink = HTMLPipeline::AutoLinkFilter.new
-newline = HTMLPipeline::NewLineFilter.new
-
 pipeline = HTMLPipeline.new(text_filters: [HTMLPipeline::AutoLinkFilter.new, HTMLPipeline::NewLineFilter.new])
 
 if ARGV[0] == nil
