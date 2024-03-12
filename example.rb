43535ae23b8378de120f377bec7a7f5db52606da
Yu-Hoto <yu.hoto.0220@gmail.com>
2024-03-12 12:45:19 +0900

    fix filter order.

--- example.rb
+++ example.rb
@@ -3,10 +3,10 @@ require './auto_link_filter.rb'
 require './new_line_filter.rb'
 require './maximum_char_filter.rb'
 
-pipeline = HTMLPipeline.new(text_filters: [HTMLPipeline::AutoLinkFilter.new, HTMLPipeline::NewLineFilter.new, HTMLPipeline::MaximumCharFilter.new])
+pipeline = HTMLPipeline.new(text_filters: [HTMLPipeline::NewLineFilter.new, HTMLPipeline::MaximumCharFilter.new, HTMLPipeline::AutoLinkFilter.new])
 
 if ARGV[0] == nil
-  example_text = "Hello \n\n\n https://google.com!!! this text is very long. but, this pipeline output max 100 characters."
+  example_text = "Hello \n\n\n https://google.com!!! this text is very long. but, this pipeline output max 100 characters. over 100 character text adjust 100 character text."
 else
   example_text = ARGV[0].gsub(/ *\\n+/, "\n")
 end
